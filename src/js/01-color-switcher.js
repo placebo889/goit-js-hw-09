cdf573afd225d87c3a80f61939d056f94af0f397
placebo889 <hentalia2d@gmail.com>
2023-11-05 16:51:22 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+ let intervalId;
+
+    function getRandomHexColor() {
+      return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+    }
+
+    function changeBackgroundColor() {
+      const newColor = getRandomHexColor();
+      document.body.style.backgroundColor = newColor;
+    }
+
+    function startBackgroundChange() {
+      intervalId = setInterval(changeBackgroundColor, 2000);
+    }
+
+    function stopBackgroundChange() {
+      clearInterval(intervalId);
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+      const startButton = document.querySelector('[data-start]');
+      const stopButton = document.querySelector('[data-stop]');
+
+      startButton.addEventListener('click', startBackgroundChange);
+      stopButton.addEventListener('click', stopBackgroundChange);
+    });
