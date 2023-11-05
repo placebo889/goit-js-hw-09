33508199a71d21958caedd86e2634791d1efc9ae
placebo889 <hentalia2d@gmail.com>
2023-11-05 17:04:04 +0200

    finish

---
+++ src/js/02-timer.js
@@ -0,0 +1,82 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css'; 
+import { Notify } from 'notiflix/build/notiflix-notify-aio'; 
+
+const startBtn = document.querySelector('[data-start]');
+const daysRef = document.querySelector('[data-days]');
+const hoursRef = document.querySelector('[data-hours]');
+const minutesRef = document.querySelector('[data-minutes]');
+const secondsRef = document.querySelector('[data-seconds]');
+let timerId = null; 
+startBtn.setAttribute('disabled', true); 
+startBtn.classList.add('button-disabled');
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+const addLeadingZero = value => String(value).padStart(2, 0);
+
+const options = {
+  enableTime: true, 
+  time_24hr: true,
+  defaultDate: new Date(), 
+  minuteIncrement: 1, // 
+  onClose(selectedDates) { 
+    if (selectedDates[0] < new Date()) { 
+      Notify.failure('Please choose a date in the future'); 
+      return;
+    }
+    startBtn.removeAttribute('disabled'); 
+    startBtn.classList.remove('button-disabled');
+
+    const showTimer = () => {
+      const now = new Date();
+      localStorage.setItem('CountDown-timer', selectedDates[0]);
+      const selectData = new Date(localStorage.getItem('CountDown-timer'));
+
+      if (!selectData) return;
+
+      const diff = selectData - now;
+      const { days, hours, minutes, seconds } = convertMs(diff);
+      daysRef.textContent = days;
+      hoursRef.textContent = addLeadingZero(hours);
+      minutesRef.textContent = addLeadingZero(minutes);
+      secondsRef.textContent = addLeadingZero(seconds);
+
+      if (
+        daysRef.textContent === '0' &&
+        hoursRef.textContent === '00' &&
+        minutesRef.textContent === '00' &&
+        secondsRef.textContent === '00'
+      ) {
+        clearInterval(timerId);
+        Notify.success('Timer Ok!!!');
+      }
+    };
+
+    const onClick = () => { 
+      if (timerId) {
+        clearInterval(timerId);
+      }
+      startBtn.setAttribute('disabled', true);
+      startBtn.classList.add('button-disabled');
+      showTimer(); 
+      timerId = setInterval(showTimer, 1000);
+    };
+
+    startBtn.addEventListener('click', onClick);
+  },
+};
+
+flatpickr('#datetime-picker', { ...options });
